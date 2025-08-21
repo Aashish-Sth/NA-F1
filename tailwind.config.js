fe15f0ae7df4d7e7cf1a55460a7dd2913b5a9a0f
AarshnavKC <aarshnavkc2@gmail.com>
2025-08-22 00:10:48 +0545

    added schedule

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
+module.exports = {
+  content: [
+    "./*.{html,js}",
+    "./src/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
